7c4fa181de8e1ebd7707d8ee5758629c8992c804
kperkins97 <perkins.kelsey@yahoo.com>
2022-01-09 13:44:21 -0600

    added plugins and loaders in config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,13 +17,37 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'Client Server',
+        template: './index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+      }), 
     ],
-
+// TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
 };
+
+//WHAT I ADDED:
+//#21 - 27
+// #32 - 43
